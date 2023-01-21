8c543f38767ef2bc1672f55f0367952117859fee
UnluckyTraki <kswarta23@gmail.com>
2023-01-21 21:21:24 +0100

    zadanie dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Wiktor";
+char sex = 'M';
+int age = 28;
+
+if (sex == 'K' && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat.");
+}
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33.");
+}
+else if (age < 18 && sex == 'M')
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
+else if (name == "Wiktor" && sex == 'M' && age < 30)
+{
+    Console.WriteLine("Wiktor, lat 28, płeć: mężczyzna.");
+}
